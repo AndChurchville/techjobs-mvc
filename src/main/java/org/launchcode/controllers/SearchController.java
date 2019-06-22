e0a4bc337f4e610c49110f69369b3f322a38ebe5
Andrea Churchville <dreabeth24@gmail.com>
2019-06-21 23:20:32 -0500

    Added code to display all jobs. Added same method for list function using fragments.

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,6 +4,7 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -24,4 +25,21 @@ public class SearchController {
 
     // TODO #1 - Create handler to process search request and display results
 
+    @RequestMapping(value = "results", method = RequestMethod.POST)
+    public String search(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        if (searchType.equals("all")) {
+            ArrayList<HashMap<String, String>> jobs = JobData.findAll();
+            model.addAttribute("jobs", jobs);
+            model.addAttribute("searchTerm", searchTerm);
+            model.addAttribute("searchType", searchType);
+            return "search";
+        } else {
+            ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("jobs", jobs);
+            model.addAttribute("searchTerm", searchTerm);
+            model.addAttribute("searchType", searchType);
+            model.addAttribute("columns", ListController.columnChoices);
+            return "search";
+        }
+    }
 }
